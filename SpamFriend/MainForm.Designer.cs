f6f1bab7a46e9401bfd53bbda7e23a209347cb0a
furuya02 <sin@sapporoworks.ne.jp>
2013-07-01 06:03:46 +0900

    2013.07.01 0600

---
+++ SpamFriend/MainForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace SpamFriend {
+    partial class MainForm {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listView = new System.Windows.Forms.ListView();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 538);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(740, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.buttonBack);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonStop);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonSearch);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxUrl);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(740, 538);
+            this.splitContainer1.SplitterDistance = 65;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(128, 10);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(39, 23);
+            this.buttonBack.TabIndex = 13;
+            this.buttonBack.Text = "<<";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.ForeColor = System.Drawing.Color.Red;
+            this.buttonStop.Location = new System.Drawing.Point(173, 11);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(37, 23);
+            this.buttonStop.TabIndex = 12;
+            this.buttonStop.Text = "●";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(12, 10);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(113, 23);
+            this.buttonSearch.TabIndex = 11;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(216, 13);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(471, 19);
+            this.textBoxUrl.TabIndex = 10;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.panel1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.listView);
+            this.splitContainer2.Size = new System.Drawing.Size(740, 469);
+            this.splitContainer2.SplitterDistance = 513;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.webBrowser);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(3);
+            this.panel1.Size = new System.Drawing.Size(513, 469);
+            this.panel1.TabIndex = 0;
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(503, 459);
+            this.webBrowser.TabIndex = 0;
+            this.webBrowser.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser_DocumentCompleted);
+            this.webBrowser.NewWindow += new System.ComponentModel.CancelEventHandler(this.webBrowser_NewWindow);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listView
+            // 
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.Location = new System.Drawing.Point(0, 0);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(223, 469);
+            this.listView.SmallImageList = this.imageList;
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.SmallIcon;
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(126, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 560);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "MainForm";
+            this.Text = "SpamFriend";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ImageList imageList;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ SpamFriend/MainForm.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SpamFriend{
+    public partial class MainForm : Form{
+
+        private int _size;
+        private Color _color;
+
+
+        public MainForm(){
+            InitializeComponent();
+
+            //const string topUrl = "https://www.facebook.com/toshiyuki.katayama.50/friends";
+            const string topUrl = "https://www.facebook.com/";
+
+            textBoxUrl.Text = topUrl;
+            timer1.Interval = 1000; //1秒
+        }
+
+
+        //友達一覧検索
+        private void SearchFriend(){
+            if (webBrowser.Document == null){
+                return;
+            }
+            var body = webBrowser.Document.Body;
+            var s = body.InnerHtml;
+            if (s == null){
+                return;
+            }
+            var start = s.LastIndexOf("友達を検索");
+            if (start < 0){
+                return;
+            }
+            var target = s.Substring(start);
+            int count = 0;
+            while (true){
+                int i = target.IndexOf("src=\"");
+                if (i == -1){
+                    break;
+                }
+                target = target.Substring(i);
+                i = target.IndexOf("\">");
+                if (i == -1){
+                    break;
+                }
+                var url = target.Substring(5, i - 5);
+                try{
+                    var ext = Path.GetExtension(url);
+                    if (ext != null && ext.ToUpper() != ".JPG"){
+                        target = target.Substring(i);
+                        continue;
+                    }
+                } catch (Exception){
+                    target = target.Substring(i);
+                    continue;
+                }
+                target = target.Substring(i);
+
+
+
+                i = target.IndexOf("/ajax/hovercard/user.php?");
+                if (i == -1){
+                    break;
+                }
+                target = target.Substring(i);
+                if (-1 == (i = target.IndexOf(">"))){
+                    break;
+                }
+                target = target.Substring(i);
+                i = target.IndexOf("<");
+                if (i == -1){
+                    break;
+                }
+
+                var name = target.Substring(1, i - 1);
+
+
+//                var imgFile = "$.JPG";
+//                var wc = new WebClient();
+//                wc.DownloadFile(url, imgFile);
+//                wc.Dispose();
+//
+//                var img = Image.FromFile(imgFile);
+//                imageList.Images.Add(img);
+//                img.Dispose();
+//
+//                listView.Items.Add(name, imageList.Images.Count - 1);
+                listView.Items.Add(name,-1);
+
+
+                target = target.Substring(i);
+
+            }
+        }
+
+
+        //新しいURLに変化した場合、TextBoxを書き換える
+        private void webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e) {
+            //webBrowser.Document.Body.Style = "zoom:80%"; 
+            var url = e.Url.ToString();
+            if (url.IndexOf("ai.php?") == -1 && url.IndexOf("/ajax/") == -1) {
+                
+                if (url.IndexOf("profile.php?id=")!=-1){
+                    int i = url.IndexOf("&");
+                    if (i != -1) {
+                        textBoxUrl.Text = url.Substring(0, i);
+                        listView.Items.Clear();
+                    }
+                } else{
+                    int i = url.IndexOf("?");
+                    if (i != -1) {
+                        textBoxUrl.Text = url.Substring(0, i);
+                        listView.Items.Clear();
+                    }
+                }
+                
+            }
+        }
+
+
+        //タイマー処理 1秒後にサイズが変わっていたら下スクロールしてAjaxを起動させる
+        private void timer1_Tick(object sender, EventArgs e){
+            if (webBrowser.Document == null){
+                return;
+            }
+            if (webBrowser.Document.Body == null){
+                return;
+            }
+            if (webBrowser.Document.Body.InnerHtml == null){
+                return;
+            }
+            var len = webBrowser.Document.Body.InnerText.Length;
+            if (len != _size){
+                _size = len;
+                webBrowser.Document.Window.ScrollTo(0, 500000);
+            } else{
+                Stop();
+            }
+
+        }
+
+
+        //textBoxのURLでブラウジング開始(スクロールして最後まで取得する)
+        private void Start(){
+
+            //listBox.Items.Clear();
+            listView.Items.Clear();
+
+            if (textBoxUrl.Text.IndexOf("profile.php?id=") != -1){
+                if (textBoxUrl.Text.IndexOf("&sk=friends") == -1) {
+                    textBoxUrl.Text = textBoxUrl.Text + @"&sk=friends";
+                }
+            } else {
+                if (textBoxUrl.Text.IndexOf("/friends") == -1) {
+                    textBoxUrl.Text = textBoxUrl.Text + @"/friends";
+                }
+            }
+
+
+
+            //ボタン状態
+            buttonSearch.Enabled = false;
+            buttonStop.Enabled = true;
+            _color = panel1.BackColor;
+            panel1.BackColor = Color.Red;
+
+            webBrowser.Navigate(textBoxUrl.Text);
+            _size = 0;
+            timer1.Enabled = true;
+        }
+
+        private void Stop(){
+            timer1.Enabled = false;
+            //スクロールを一番上に戻す
+            webBrowser.Document.Window.ScrollTo(0, 0);
+
+            //ボタン状態
+            buttonSearch.Enabled = true;
+            buttonStop.Enabled = false;
+            panel1.BackColor = _color;
+
+            //友達一覧
+            SearchFriend();
+
+            toolStripStatusLabel1.Text = String.Format("すべての友達 {0}",listView.Items.Count);
+
+            //デバッグため色を付けてみる
+            if (listView.Items.Count > 0){
+                ListViewItem lvItem = listView.Items[0];
+                lvItem.ForeColor = Color.Red;
+            }
+
+
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e){
+            webBrowser.GoBack();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e){
+            Start();
+
+        }
+
+
+        private void buttonStop_Click(object sender, EventArgs e){
+            webBrowser.Stop();
+            Stop();
+
+        }
+
+        private void webBrowser_NewWindow(object sender, System.ComponentModel.CancelEventArgs e){
+            int x = 0;
+        }
+
+    }
+}
